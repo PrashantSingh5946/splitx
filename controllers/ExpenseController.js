08295aeff3e781b2f75d2025c8dc8a1692c12205
Prashant Singh <octanesingh@gmail.com>
2023-10-30 22:30:19 +0530

    CRUD for Expense controller complete

--- controllers/ExpenseController.js
+++ controllers/ExpenseController.js
@@ -2,6 +2,7 @@ const Expense = require("../models/ExpenseModel");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const GroupModel = require("../models/GroupModel");
+const ExpenseModel = require("../models/ExpenseModel");
 
 //Create an expense
 
@@ -35,19 +36,159 @@ module.exports.Add = async (req, res, next) => {
     // Abort the transaction.
     await session.abortTransaction();
     console.log(error);
-    res.send(500, "Error occurred");
+    res.status(500);
     // Throw the error.
   } finally {
     // End the session.
     await session.endSession();
   }
-
-  //Complete the request
-  res.send("Expense added");
+  res.send({ status: true, message: "Expense added" });
 };
 
 //Read an expense
 
+module.exports.Get = async (req, res) => {
+  let expenseData;
+  const session = await mongoose.startSession();
+  try {
+    const { id: expenseId } = req.params;
+
+    const token = req.cookies.token;
+    const { id: user_id } = jwt.verify(token, process.env.TOKEN_KEY);
+
+    await session.withTransaction(async () => {
+      //Find the expense
+      let expense = await Expense.findOne({ _id: expenseId, ownerId: user_id });
+
+      if (!expense) {
+        throw "Expense does not exist";
+      }
+
+      //Find the group associated
+      let group = await GroupModel.findOne({ _id: expense.groupId });
+
+      if (!group) {
+        throw "Group does not exist";
+      }
+
+      let { groupMembers } = group;
+
+      if (groupMembers.includes(user_id)) {
+        expenseData = expense;
+      }
+    });
+
+    await session.commitTransaction();
+  } catch (error) {
+    // Abort the transaction.
+    await session.abortTransaction();
+    console.log(error);
+    res.status(500);
+    // Throw the error.
+  } finally {
+    await session.endSession();
+  }
+  res.send(expenseData);
+};
+
 //Update an expense
+module.exports.Update = async (req, res) => {
+  console.log("Session started");
+  const session = await mongoose.startSession();
+
+  try {
+    console.log("Data passed ", req.body);
+    const { id: expense_id } = req.params;
+    const token = req.cookies.token;
+
+    console.log(expense_id);
+
+    const { id } = jwt.verify(token, process.env.TOKEN_KEY);
+    // Start the transaction.
+    await session.withTransaction(async () => {
+      const result = await ExpenseModel.updateOne(
+        { _id: expense_id },
+        {
+          $set: {
+            ...req.body,
+          },
+        }
+      );
+    });
+
+    // Commit the transaction.
+    await session.commitTransaction();
+  } catch (error) {
+    // Abort the transaction.
+    await session.abortTransaction();
+    console.log(error);
+    res.status(500);
+    // Throw the error.
+  } finally {
+    // End the session.
+    await session.endSession();
+  }
+  res.send({ status: true, message: "Expense Updated " });
+};
 
 //Delete an expense
+
+module.exports.Delete = async (req, res) => {
+  let expenseData;
+  const session = await mongoose.startSession();
+  try {
+    const { id: expenseId } = req.params;
+
+    const token = req.cookies.token;
+    const { id: user_id } = jwt.verify(token, process.env.TOKEN_KEY);
+
+    await session.withTransaction(async () => {
+      //Find the expense
+      let expense = await Expense.findOne({ _id: expenseId, ownerId: user_id });
+
+      if (!expense) {
+        throw "Expense does not exist";
+      }
+
+      //Find the group associated
+      let group = await GroupModel.findOne({ _id: expense.groupId });
+
+      if (!group) {
+        throw "Group does not exist";
+      }
+
+      let { groupMembers } = group;
+
+      if (!groupMembers.includes(user_id)) {
+        throw "User doesnt have access on the expense group";
+      }
+
+      //Start deleting the expense
+
+      //Remove from group expenses array
+      const result = await GroupModel.updateOne(
+        { _id: group._id },
+        { $pull: { expenses: expenseId } }
+      );
+
+      console.log(result);
+
+      //Remove the expense from expenses collection
+
+      await ExpenseModel.deleteOne({
+        _id: expenseId,
+      });
+    });
+
+    await session.commitTransaction();
+  } catch (error) {
+    // Abort the transaction.
+    await session.abortTransaction();
+    console.log(error);
+    res.status(500);
+    // Throw the error.
+  } finally {
+    await session.endSession();
+  }
+  res.send("Deleted successfully");
+};
--- routes/ExpenseRoute.js
+++ routes/ExpenseRoute.js
@@ -1,7 +1,15 @@
-const { Add } = require("../controllers/ExpenseController");
+const {
+  Add,
+  Get,
+  Delete,
+  Update,
+} = require("../controllers/ExpenseController");
 const router = require("express").Router();
 const { userVerification } = require("../middlewares/AuthMiddleware");
 
 router.post("/add", userVerification, Add);
+router.get("/get/:id", userVerification, Get);
+router.delete("/delete/:id", userVerification, Delete);
+router.put("/update/:id", userVerification, Update);
 
 module.exports = router;
